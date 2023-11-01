18f06ef11a365a6fd2678c6be4ec3b3059f26fb3
Federico <federicoc03@gmail.com>
2023-10-31 23:53:22 -0500

    feat: :sparkles: addReportRouter

--- src/pages/report/index.jsx
+++ src/pages/report/index.jsx
@@ -1,11 +1,12 @@
 import Layout from "@/components/Layout/Layout";
+import ReportForm from "@/components/Report/RepForm/ReportForm";
 
 export default function Report() {
   const title = "Reporta un peludo";
 
   return (
     <Layout title={title}>
-      <h1>Report es esto</h1>
+      <ReportForm />
     </Layout>
   );
 }
