c79ca27d7bdcd7d802b994b92bed87babd19463a
Federico Córdoba <79395978+mercuryeater@users.noreply.github.com>
2023-11-21 12:22:37 -0500

    Feature/report form (#7)
    * chore: :construction: addFormV1
    
    * feat: :sparkles: FormAlmostDone
    
    * fix: :adhesive_bandage: fixHeroOnDesktop

--- src/components/Report/RepForm/ReportForm.jsx
+++ src/components/Report/RepForm/ReportForm.jsx
@@ -11,7 +11,8 @@ export default function ReportForm() {
     "Hora",
     "Lugar",
     "Descripción",
-    "Información relevante",
+    "Nombre de contacto",
+    "Teléfono",
   ];
 
   const sendForm = (e) => {
@@ -20,17 +21,63 @@ export default function ReportForm() {
   };
   return (
     <form className={style.Form} onSubmit={sendForm}>
+      <h3>¿Es un peludo visto o perdido?</h3>
+      <div className={style.Form__radioContainer}>
+        <div className={style.Form__radio}>
+          <label className={style.Form__radio__label} htmlFor="visto">
+            <input
+              className={style.Form__radio__input}
+              type="radio"
+              id="visto"
+              value="visto"
+              name="postType"
+            />
+            <span className={style.Form__radio__checkmark} />
+            Visto
+          </label>
+        </div>
+        <div className={style.Form__radio}>
+          <label className={style.Form__radio__label} htmlFor="perdido">
+            <input
+              className={style.Form__radio__input}
+              type="radio"
+              id="perdido"
+              value="perdido"
+              name="postType"
+            />
+            <span className={style.Form__radio__checkmark} />
+            Perdido
+          </label>
+        </div>
+      </div>
+
+      {/*Aca delante este input seria chevere hacerlo mas estetico
+          con una imagen de fondo y que permita drag and drop  */}
+      <label htmlFor="images">
+        Fotos del peludo
+        <input
+          className={style.Form__inputImage}
+          id="images"
+          name="images"
+          type="file"
+          accept="image/*"
+          required
+        />
+      </label>
+
       {inputFields.map((field, index) => (
         <React.Fragment key={index}>
           <label className={style.Form__label} htmlFor={field}>
             {field}
+
+            <input
+              className={style.Form__input}
+              type="text"
+              name={field}
+              maxLength="100"
+              required
+            />
           </label>
-          <input
-            className={style.Form__input}
-            type="text"
-            name={field}
-            maxLength="100"
-          />
         </React.Fragment>
       ))}
       {/* <input
@@ -41,14 +88,19 @@ export default function ReportForm() {
         maxLength="100"
         required
       /> */}
-      {/* <textarea
-        className={style.Form__input}
-        type="text"
-        placeholder="Tell me, how can I help you?"
-        name="message"
-        maxLength="2000"
-        required
-      /> */}
+      <label className={style.Form__label} htmlFor="descripcion">
+        Descripción
+        <textarea
+          className={style.Form__textArea}
+          type="text"
+          placeholder="¿Cómo pasó todo?"
+          id="descripcion"
+          name="descripcion"
+          maxLength="2000"
+          required
+        />
+      </label>
+
       <button className={style.Form__btn} type="submit">
         ENVIAR REPORTE
       </button>
