bdc201086f7ff6d55a22d1596ceb58c2895d31ca
Federico <federicoc03@gmail.com>
2024-02-29 22:41:14 -0500

    feat:

---
+++ src/components/Details/Header/Header.jsx
@@ -0,0 +1,32 @@
+import { useState, useRef } from "react";
+import s from "./Header.module.scss";
+import { useRouter } from "next/router";
+import { FaShareFromSquare } from "react-icons/fa6";
+import { IoChevronBack } from "react-icons/io5";
+import { IoMdShare } from "react-icons/io";
+
+export default function Header() {
+  const router = useRouter();
+
+  const [sharing, setSharing] = useState(false);
+
+  // For the component fo sharing we use a ref to check clicks inside or outside
+  const sharingRef = useRef(null);
+
+  function openSharing() {
+    setSharing(!sharing);
+    console.log(`Clicked and set sharing to ${sharing}`);
+  }
+
+  return (
+    <header className={s.header}>
+      <IoChevronBack className={s.back} onClick={() => router.back()} />
+
+      {/* El share debe crear por ejemplo un JSON con toda la info del post
+      o hacer el fetch de esa info y de esta manera transportarlo a otras apps como fb, tw, etc
+      Sin emabrgo lo que se puede hacer es que se abra una hover window donde va lo de copartir
+      a diferentes paginas como fb, twitter, whatsapp, telegra, etc. Usando react-share lib */}
+      <IoMdShare className={s.share} onClick={openSharing} />
+    </header>
+  );
+}
--- src/pages/testeo/index.js
+++ src/pages/testeo/index.js
@@ -1,11 +1,17 @@
 import Layout from "@/components/Layout/Layout";
+import { useRouter } from "next/router";
 
-export default function testeo() {
+export default function Testeo() {
+  const router = useRouter();
+
   return (
     <>
       <Layout title={"title"}>
         <h1>HOLA MUNDO</h1>
         <h2>Esto es test</h2>
+        <button type="button" onClick={() => router.push("./details")}>
+          detailsDog
+        </button>
       </Layout>
     </>
   );
